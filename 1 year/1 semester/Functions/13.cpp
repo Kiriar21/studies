c43a481b4ebc6cf86974327acdfbbf65cbc3bbe6
Kiriar21 <artur.m326@gmail.com>
2023-11-20 16:43:36 +0100

    second commit
    second commit

---
+++ 1 year/1 semester/Functions/13.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double funkcja(double &a, double &b);
+int main()
+{
+    double a = 64, b = 36;
+    cout << "Wartosc sumy pierwiastkow: " << funkcja(a, b);
+    return 1;
+}
+double funkcja(double &a, double &b)
+{
+    return (sqrt(a) + sqrt(b));
+}
+// Przyklad na ekranie:
+// Dla wartosci: 64 36
+// 14
+// Dla wartosci: 25 25
+// 10
+// Dla wartosci: 1 1
+// 2
