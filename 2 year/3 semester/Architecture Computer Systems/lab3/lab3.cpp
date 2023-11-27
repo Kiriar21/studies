63cc9607ee161026c0a1289f2c2b253a01ecc4f8
Kiriar21 <artur.m326@gmail.com>
2023-11-27 16:58:30 +0100

    2y3s ACS lab3 added

---
+++ 2 year/3 semester/Architecture Computer Systems/lab3/lab3.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+int main()
+{
+
+    //Zadanie 1
+
+
+
+    // int a = 5;
+    // int b = 4;
+    // int c = 0;
+    // int d = 0;
+    // int e = 0;
+    // int f = 0;
+    // c = a + b;
+    // d = a - b;
+    // e = a * b;
+    // f = a / b;
+
+    /*
+     + addl
+     - subl
+     * imull
+     / cltd,  idivl
+    */ 
+
+    // float a = 5;
+    // float b = 4;
+    // float c = 0;
+    // float d = 0;
+    // float e = 0;
+    // float f = 0;
+    // c = a + b;
+    // d = a - b;
+    // e = a * b;
+    // f = a / b;
+
+    /*
+    + addss
+    - subss
+    * mulls
+    / divss
+    */
+
+    double a = 5;
+    double b = 4;
+    double c = 0;
+    double d = 0;
+    double e = 0;
+    double f = 0;
+    c = a + b;
+    d = a - b;
+    e = a * b;
+    f = a / b;
+
+    /*
+     + addsd
+     - subsd
+     * mulsd
+     / divsd
+    */
+
+    std::cout<<c<<std::endl;
+
+
+
+    //Zadanie 2
+
+    /*
+        Dla liczb zmiennoprzecinkowych zamiana typu operatora arytmetycznego nie robi różnicy i liczy się poprawnie.
+        Natomiast dla typu całkowitego int różnica jest. Dla dzielenia ono nie zadziała wcale. Dla mnożenia i odejmowania jest inna instrukcja (taka sama dla oby dwóch innych)
+        jednak nadal różni sie od instrukcji dodawania. 
+        
+    */
+    return 0;
+}
