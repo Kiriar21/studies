2b3caa109d3a197d653fb5955d60f95aa66b43c8
Kiriar21 <artur.m326@gmail.com>
2023-11-28 12:58:48 +0100

    2y3s ACS lab4 task3 added

---
+++ 2 year/3 semester/Architecture Computer Systems/lab4/zad3_2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdlib.h>
+#include <omp.h>
+int main() {
+    // Utworzenie tablic
+    const int n =1024*1024*128;
+    double * A = new double[n];
+    double * B = new double[n];
+    double * C = new double[n];
+    double * D = new double[n];
+    double time;
+    // Inicjalizacja danych
+    for(int i=0; i<n; ++i) {
+        A[i]=1.0*i;
+        B[i]=2.0*i;
+        C[i]=3.0*i;
+        D[i]=4.0*i;
+    }
+    time = omp_get_wtime(); // pomiar czasu - start
+    // Fragment do modyfikacji
+    for(int i=0; i<n; ++i) {
+        double pom = 1.0;
+        C[i] = A[i]+pom;
+        A[i] = D[i]+pom;
+    }
+    time = omp_get_wtime() - time; // pomiar czasu - stop
+    std::cout<<" czas wykonania = "<<time;
+    std::cout<<" przykładowa wartość "<<A[3]<<std::endl;
+    // Kasowanie danych
+    delete [] A;
+    delete [] B;
+    delete [] C;
+    delete [] D;
+    return 0;
+}
