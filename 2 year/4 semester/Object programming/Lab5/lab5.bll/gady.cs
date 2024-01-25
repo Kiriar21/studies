120456dee11097eabc68b5e940ab4829f3694938
Kiriar21 <artur.m326@gmail.com>
2024-01-25 10:31:09 +0100

    2y4s object programming lab5 added

---
+++ 2 year/4 semester/Object programming/Lab5/lab5.bll/gady.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab5.bll
+{
+    public class gady
+    {
+        public string gatunek
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string pozywienie
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string pochodzenie
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public List<string> skille
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
